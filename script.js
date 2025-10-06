b30c3e2d5aeae32335a115df22c38aa5fc6a3a80
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-10-06 08:45:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-//your JS code here. If required.
+// select all input fields using querySelectorAll in a variable
+const inputs = document.querySelectorAll('.code');
+
+// code for focus management, run a loop on inputs node list
+inputs.forEach((input, index) => {
+	// run the below code for each input
+	input.addEventListener('click', () => {
+		input.select();
+	});
+
+	
+
+	input.addEventListener('input', () => {
+		// allow only numbers
+		input.value = input.value.replace(/[^0-9]/g, '');
+		
+		if (input.value.length > 1) {
+			input.value = input.value.slice(0, 1);  // Restrict to 1 digit
+		}
+		if(input.value.length === 1){
+			if(index < inputs.length - 1){
+				inputs[index+1].focus();
+			}
+		}
+	});
+
+	input.addEventListener('keydown', (e) => {
+		if(e.key === "Backspace" && input.value.length === 0){
+			if(index > 0){
+				inputs[index - 1].focus();
+			}
+		}
+	})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
